ebd9754a3ca0444a3393fbbd13db623bc2fbf29f
EmbeddedDesign <stephen.harper@noctivore.com>
2020-06-25 18:12:50 -0700

    firmware updates

--- firmware/.dep/main.o.d
+++ firmware/.dep/main.o.d
@@ -4,8 +4,7 @@ main.o: main.c /usr/lib/avr/include/avr/power.h \
  /usr/lib/avr/include/stdint.h /usr/lib/avr/include/avr/iom328p.h \
  /usr/lib/avr/include/avr/portpins.h /usr/lib/avr/include/avr/common.h \
  /usr/lib/avr/include/avr/version.h /usr/lib/avr/include/avr/fuse.h \
- /usr/lib/avr/include/avr/lock.h /usr/lib/avr/include/util/delay.h \
- /usr/lib/avr/include/util/delay_basic.h /usr/lib/avr/include/math.h \
+ /usr/lib/avr/include/avr/lock.h /usr/lib/avr/include/avr/sleep.h \
  /usr/lib/avr/include/avr/interrupt.h /usr/lib/avr/include/stdio.h \
  /usr/lib/gcc/avr/5.4.0/include/stdarg.h \
  /usr/lib/gcc/avr/5.4.0/include/stddef.h main.h uart.h
@@ -34,11 +33,7 @@ main.o: main.c /usr/lib/avr/include/avr/power.h \
 
 /usr/lib/avr/include/avr/lock.h:
 
-/usr/lib/avr/include/util/delay.h:
-
-/usr/lib/avr/include/util/delay_basic.h:
-
-/usr/lib/avr/include/math.h:
+/usr/lib/avr/include/avr/sleep.h:
 
 /usr/lib/avr/include/avr/interrupt.h:
 
--- firmware/main.c
+++ firmware/main.c
@@ -5,7 +5,7 @@
 
  // Include headers
 #include <avr/power.h>
-#include <util/delay.h>
+#include <avr/sleep.h>
 #include <avr/interrupt.h>
 #include <stdio.h>
 #include "main.h"
@@ -16,8 +16,8 @@
 #define PARK_RIGHT   15 // A1
 #define PARK_LEFT    16 // A2
 #define PARK_REAR    17 // A3
-#define POWER_STATUS 5
-#define PARK_STATUS  6
+#define PARK_STATUS  18
+#define POWER_STATUS 13
 #define ACC          2
 
 // Enable Park Mode
@@ -27,7 +27,7 @@ void enableParkMode(void) {
   PORTC &= ~_BV(PORTC2); // digitalWrite(PARK_LEFT, LOW);
   PORTC &= ~_BV(PORTC3); // digitalWrite(PARK_REAR, LOW);
   // Turn PARK_STATUS LED ON
-  PORTD &= ~_BV(PORTD6); // digitalWrite(PARK_STATUS, LOW);
+  PORTC |= _BV(PORTC4); // digitalWrite(PARK_STATUS, HIGH);
   puts("Park Mode: Enabled"); // Print park mode status message
 }
 
@@ -38,30 +38,19 @@ void disableParkMode(void) {
   PORTC |= _BV(PORTC2); // digitalWrite(PARK_LEFT, HIGH);
   PORTC |= _BV(PORTC3); // digitalWrite(PARK_REAR, HIGH);
   // Turn PARK_STATUS LED OFF
-  PORTD |= _BV(PORTD6); // digitalWrite(PARK_STATUS, HIGH);
+  PORTC &= ~_BV(PORTC4); // digitalWrite(PARK_STATUS, LOW);
   puts("Park Mode: Disabled"); // Print park mode status message
 }
 
 // Main loop
 int main(void) {
-  // Power saving
-  ADCSRA = 0;  // Disable ADC
-  SPCR = 0; // Disable SPI
-  power_adc_disable ();
-  power_spi_disable();
-  power_twi_disable();
-  power_timer1_disable();
-  // Turn off brown-out enable in software
-  // MCUCR = bit (BODS) | bit (BODSE);
-  // MCUCR = bit (BODS);
-
-  // Configure IO pins
+  // Configure IO
   DDRC |= _BV(DDC0);    // pinMode(PARK_FRONT, OUTPUT);
   DDRC |= _BV(DDC1);    // pinMode(PARK_RIGHT, OUTPUT);
   DDRC |= _BV(DDC2);    // pinMode(PARK_LEFT, OUTPUT);
   DDRC |= _BV(DDC3);    // pinMode(PARK_REAR, OUTPUT);
-  DDRD |= _BV(DDD5);    // pinMode(POWER_STATUS, OUTPUT);
-  DDRD |= _BV(DDD6);    // pinMode(PARK_STATUS, OUTPUT);
+  DDRC |= _BV(DDC4);    // pinMode(PARK_STATUS, OUTPUT);
+  DDRB |= _BV(DDB5);    // pinMode(POWER_STATUS, OUTPUT);
   DDRD &= ~_BV(DDD2);   // Clear the PD2 pin
   PORTD |= _BV(PORTD2); // Enable PD2 internal pull-up
   
@@ -70,22 +59,41 @@ int main(void) {
   stdout = &uart_output;
   stdin  = &uart_input;
 
-  // Interupt init
-  // Enable pin change interrupt on the PCINT18 pin using Pin Change Mask Register 2 (PCMSK2)
-  PCMSK2 |= _BV(PCINT18);
-  // Enable pin change interrupt 2 using the Pin Change Interrrupt Control Register (PCICR)
-  PCICR |= _BV(PCIE2);
-  // Turn on interrupts
-  sei();
-
   // Turn POWER_STATUS LED ON
-  PORTD &= ~_BV(PORTD5); // digitalWrite(PARK_STATUS, LOW);
+  PORTB |= _BV(PORTB5); // digitalWrite(POWER_STATUS, HIGH);
   // Print system status message
   puts("System Status: OK");
  
  // Loop
   while(1) {
-    // Nothing to do here
+    // Power saving
+    power_adc_disable ();
+    power_spi_disable();
+    power_twi_disable();
+    power_timer1_disable();
+    power_timer2_disable();
+
+    // Configure sleep
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_enable();
+
+    // Do not interrupt before to sleep or ISR will detach interrupts won't wake.
+    cli();
+
+    // Interrupt init
+    // Enable Pin Change Interrupt on the PCINT18 pin using Pin Change Mask Register 2 (PCMSK2)
+    PCMSK2 |= _BV(PCINT18);
+    // Enable Pin Change Interrupt 2 using the Pin Change Interrupt Control Register (PCICR)
+    PCICR |= _BV(PCIE2);
+
+    // Turn off brown-out enable in software
+    MCUCR = _BV(BODS) | _BV(BODSE);
+    MCUCR = _BV(BODS);
+
+    // Enable interrupts
+    sei();
+    // Guaranteed sleep_cpu call will be done as CPU executes next instruction after interrupts enabled
+    sleep_cpu();
   }
 
   return 0;
@@ -93,6 +101,10 @@ int main(void) {
 
 ISR(PCINT2_vect)
 {
+  // Cancel sleep as a precaution
+  sleep_disable();
+  // Disable Pin Change Interrupt 2 while we do other stuff
+  PCICR &= ~_BV(PCIE2);
   // Read PD2 using the Port D Pin Input Register (PIND)
   if (PIND & _BV(PIND2)) { // PD2 is HIGH
     enableParkMode();
